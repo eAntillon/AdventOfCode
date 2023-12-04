454195e6e97db0f6de30531f08b2c7c3ca1b10a9
Erick Antillón <jpj007.ek@gmail.com>
2023-12-04 14:58:59 -0600

    first part day 3

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,17 +1,15 @@
 import re
 
-data = open('./input/day3.txt', 'r').readlines()
+data = open("./input/day3.txt", "r").read().split("\n")
 lines = len(data)
-cols = len(data[0])
 i = 0
 acc = 0
 while i < lines:
     j = 0
-    buff = ''
+    buff = ""
     first_j = 0
     last_j = 0
-    while j < cols:
-        print(f" i{i} j{j}")
+    while j < len(data[i]):
         char = data[i][j]
         first_j = j
         if char.isnumeric():
@@ -20,38 +18,51 @@ while i < lines:
                     buff += char
                     j += 1
                     last_j = j
+                    if j >= len(data[i]):
+                        break
                     char = data[i][j]
                 else:
                     break
-            print(f"number: {buff} i:{i} f:{first_j} l:{last_j}")
-            # search for adyacent symbol in 
+            print(f"number: {buff} line:{i} f:{first_j} l:{last_j}")
+            # search for adyacent symbol in
             #   [i-1][first_j-1 --- last_j+1]
             #   [i][first_j-1]    [i][last_j+1]
             #   [i+1][first_j-1 --- last_j+1]
+
+            valid_number = False
             if i > 0:
-                if re.search(r"[^.\d]",data[i-1][first_j-1:last_j+1]) is not None:
-                    acc += int(buff)
-                    buff = ''
-                    continue
-            if first_j > 0:
+                f = first_j
+                l = last_j
+                if first_j > 0: f-=1
+                if last_j < len(data[i]): l+=1 
+                if (
+                    re.search(r"[^.\d]", data[i - 1][f:l])
+                    is not None
+                ):
+                    print(f"valid {buff} {data[i - 1][f:l]} i:{i-1} {first_j}  {last_j}")
+                    valid_number = True
+            if first_j > 0 and not valid_number:
                 if re.search(r"[^.\d]", data[i][first_j-1]) is not None:
-                    acc += int(buff)
-                    buff = ''
-                    continue
-            try:
-                if last_j < cols-1:
-                    if re.search(r"[^.\d]", data[i][last_j+1]) is not None:
-                        acc += int(buff)
-                        buff = ''
-                        continue
-            except Exception:
-                print(f"lastj error {last_j} cols: {cols}" )
-            if i < lines -1:
-                if re.search(r"[^.\d]", data[i+1][first_j-1:last_j+1]) is not None:
-                    acc += int(buff)
-                    buff = ''
-                    continue
+                    print(f"valid {buff} {data[i][first_j-1]}    i:{i} {first_j}  {last_j}")
+                    valid_number = True
+            if last_j < len(data[i]) and not valid_number:
+                if re.search(r"[^.\d]", data[i][last_j]) is not None:
+                    print(  
+                        f"valid {buff} {data[i][last_j]}    i:{i} {first_j}  {last_j}"
+                    )
+                    valid_number = True
+            if i + 1 < lines and not valid_number:
+                if first_j > 0: first_j-=1
+                if last_j < len(data[i]): last_j+=1 
+                if (
+                    re.search(r"[^.\d]", data[i + 1][first_j: last_j])
+                    is not None
+                ):
+                    print(f"valid {buff} {data[i + 1][first_j: last_j]}")
+                    valid_number = True
+            if valid_number:
+                acc += int(buff)
+            buff = ""
         j += 1
     i += 1
 print(acc)
-        
