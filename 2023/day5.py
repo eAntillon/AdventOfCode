ddf1f0d48ce22a1c968b06c718ca2bd455bf4fdd
Erick Antillón <jpj007.ek@gmail.com>
2023-12-07 10:03:38 -0600

    07-12-2023 solution

--- 2023/day5.py
+++ 2023/day5.py
@@ -2,10 +2,36 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
-data = open("./input/day5_test.txt", "r").read().split("\n\n")
+
+# def mapSeeds(seeds):
+#     current_seeds = seeds.copy()
+#     for map in maps:
+#         edited_seeds = [0 for x in range(len(current_seeds))]
+#         for line in map:
+#             output, start, length = line[0], line[1], line[2]
+#             for i in range(len(current_seeds)):
+#                 seed = current_seeds[i]
+#                 if seed >= start and seed < start + length and edited_seeds[i] == 0:
+#                     current_seeds[i] = output + (seed - start)
+#                     edited_seeds[i] = 1
+
+#     return min(current_seeds)
+
+# # part 1
+# print("Part 1:", mapSeeds(seeds))
+
+# part 2
+
+
+import sys
+import time
+from multiprocessing import Pool
+from signal import signal, SIGINT
+from sys import exit
+
+data = open("./input/day5.txt", "r").read().split("\n\n")
 seeds = data[0].split(":")[1].strip().split(" ")
 seeds = [int(x) for x in seeds]
-
 maps = []
 for map in data[1:]:
     text_map = map.split("\n")[1:]
@@ -19,120 +45,69 @@ for map in data[1:]:
         # print(f"--- {new_set} ---")
     maps.append(new_map)
 
-
-def mapSeeds(seeds):
-    current_seeds = seeds.copy()
-    for map in maps:
-        edited_seeds = [0 for x in range(len(current_seeds))]
-        for line in map:
-            output, start, length = line[0], line[1], line[2]
-            for i in range(len(current_seeds)):
-                seed = current_seeds[i]
-                if seed >= start and seed < start + length and edited_seeds[i] == 0:
-                    current_seeds[i] = output + (seed - start)
-                    edited_seeds[i] = 1
-
-    return min(current_seeds)
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print("SIGINT or CTRL-C detected. Exiting gracefully")
+    exit(0)
 
 
-# part 1
-print("Part 1:", mapSeeds(seeds))
+def getSeedLocation(seeds_range):
+    print(f"\033[91m###### Working on seed range {seeds_range} \033[0m")
+    min = sys.maxsize
+    signal(SIGINT, handler)
 
-# part 2
-p2_seeds = []
-
-
-class Range:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.processed = False
-
-    def __repr__(self):
-        return f"Range({self.start}, {self.end})"
-
-
-for i in range(len(seeds)):
-    if (i + 1) % 2 == 0 and i > 0:
-        p2_seeds.append(Range(seeds[i - 1], seeds[i - 1] + seeds[i]))
-
-p2_seeds.sort(key=lambda x: x.start)
-print("\033[92m###### INITIAL SEEDS RANGES \033[0m")
-for seed in p2_seeds:
-    print(seed.start, seed.end)
-
-print("\033[92m###### \033[0m")
-
-
-def mapSeedsRange(seeds):
-    current_seeds = seeds.copy()
-
-    # map filter
-    for map in maps:
-        # line of the map
-        for seed in current_seeds:
-            seed.processed = False
-        for line in map:
-            print("\033[95m###### LINE MAP \033[0m")
-            print(line)
-            print("\033[95m###### \033[0m")
-            output, start, length = line[0], line[1], line[2]
-            for i in range(len(current_seeds)):
-                current_range = current_seeds[i]
-                range_start, range_end, processed = (
-                    current_range.start,
-                    current_range.end,
-                    current_range.processed,
-                )
-                print(f"\033[91m###### WORKING RANGE {current_range} \033[0m")
-                if range_start >= start and processed is False:
-                    if range_end < start + length:
-                        # if the current range fit inside the current line
-                        # transform the start and end of the range of the current line
-                        print(
-                            f"\033[93m###### VALID RANGE {current_range} fit ({start},{start+length}) \033[0m"
-                        )
-                        converted_range = Range(
-                            output + (range_start - start),
-                            output + (range_end - start),
-                        )
-                        converted_range.processed = True
-                        current_seeds[i] = converted_range
-                        print(f"\033[93m###### converter to {converted_range}\033[0m")
-                    else:
-                        # else, if the current range start fits, but the end doesn't
-                        # split the current range in two and insert the second part after the current one
-                        print(
-                            f"\033[94m###### EVALUATE RANGE ({start},{start+length}) \033[0m"
-                        )
-
-                        valid_range = Range(
-                            range_start,
-                            start + length - 1,
-                        )
-                        end_cut = start + length - 1
-
-                        print(
-                            f"\033[93m###### SPLIT RANGE {current_range} in {valid_range} and {Range(end_cut, range_end)} \033[0m"
-                        )
-                        valid_range = Range(
-                            output + (range_start - start), (output + length)
-                        )
-                        new_range = Range(end_cut, range_end)
-                        valid_range.processed = True
-                        current_seeds[i] = valid_range
-                        current_seeds.insert(i, new_range)
-                else:
-                    print(
-                        f"\033[93m###### INVALID RANGE {current_range} not in ({start},{start+length}) \033[0m"
-                    )
-        # print("\033[91m###### OUTPUT RANGES \033[0m")
-        # pp.pprint(current_seeds)
-    # return de min of the start of the ranges
-    current_seeds.sort(key=lambda x: x.start)
-    print(current_seeds)
-    return current_seeds[0].start
+    for seed in range(seeds_range[0], seeds_range[1]):
+        # print(f"\033[91m###### Working on seed {seed} \033[0m")
+        current_value = seed
+        # print(f"Maping {seed}")
+        for map in maps:
+            processed = False
+            # print("\033[95m###### LINE MAP \033[0m")
+            # print(map)
+            for line in map:
+                output, start, length = line[0], line[1], line[2]
+                if (
+                    current_value >= start
+                    and current_value < start + length
+                    and not processed
+                ):
+                    # print(f"Maped {current_value} to", end=" ")
+                    # print(f" {current_value} in {line}")
+                    current_value = output + (current_value - start)
+                    processed = True
+                    break
+        # print(f"\033[91m###### Result {current_value} \033[0m")
+        if current_value < min:
+            # print(f"Maped {seed} to {current_value}")
+            min = current_value
+            # print progress
+            print(f"\033[92m###### Worked {current_value} {min} \033[0m")
+    return min
 
 
-print("Part 2:", mapSeedsRange(p2_seeds[0:1]))
-# print("Part 2:", mapSeeds(p2_seeds))
+start_time = time.time()
+if __name__ == "__main__":
+    p2_seeds = []
+    for i in range(len(seeds)):
+        if (i + 1) % 2 == 0 and i > 0:
+            p2_seeds.append([seeds[i - 1], seeds[i - 1] + seeds[i]])
+    print("\033[92m###### INITIAL SEEDS RANGES \033[0m")
+    for seed in p2_seeds:
+        print(seed)
+    for i in range(len(p2_seeds)):
+        seed_range = p2_seeds[i]
+        middle = (seed_range[1] + seed_range[0]) // 2
+        p2_seeds[i] = [seed_range[0], middle]
+        p2_seeds.insert(i + 1, [middle + 1, seed_range[1]])
+    print("\033[92m###### SPLITED SEEDS RANGES \033[0m")
+    for seed in p2_seeds:
+        print(seed)
+
+
+    with Pool(8) as pool:
+        results = pool.imap(getSeedLocation, p2_seeds)
+        # print(results)
+        # pool.terminate()
+        # pool.join()
+        print("Part 2:", min(results))
+    print("--- %s seconds ---" % (time.time() - start_time))
---
+++ 2023/day7.py
@@ -0,0 +1,133 @@
+input = open("./input/day7.txt", "r").read().split("\n")
+
+rank = {
+    "2": 1,
+    "3": 2,
+    "4": 3,
+    "5": 4,
+    "6": 5,
+    "7": 6,
+    "8": 7,
+    "9": 8,
+    "T": 9,
+    "J": 10,
+    "Q": 11,
+    "K": 12,
+    "A": 13,
+}
+
+hands = {
+    "one pair": 1,  # 1
+    "two pairs": 2,  # 2
+    "three of a kind": 3,  # 3
+    "full house": 4,  # 3 y 2
+    "four of a kind": 5,  # 4
+    "five of a kind": 6,  # 5
+}
+
+sorted_plays = []
+cards_arr = []
+for line in input:
+    cards, bid = line.split(" ")
+    cards = [*cards]
+    bid = int(bid)
+    hand = 0
+    cards_count = {}
+    strength_per_position = []
+    for card in cards:
+        if card in cards_count:
+            cards_count[card] += 1
+        else:
+            cards_count[card] = 1
+        strength_per_position.append(rank[card])
+
+    card_counts_values = list(cards_count.values())
+    if 5 in card_counts_values:
+        hand = hands["five of a kind"]
+    elif 4 in card_counts_values:
+        hand = hands["four of a kind"]
+    elif 3 in card_counts_values:
+        if 2 in card_counts_values:
+            hand = hands["full house"]
+        else:
+            hand = hands["three of a kind"]
+    elif 2 in card_counts_values:
+        if card_counts_values.count(2) == 2:
+            hand = hands["two pairs"]
+        else:
+            hand = hands["one pair"]
+    else:
+        hand = 0
+    # print(f" c:{cards} h:{hand} b:{bid} spp:{strength_per_position}")
+    cards_arr.append((bid, hand, strength_per_position, cards))
+
+    sorted_plays.append((bid, hand, strength_per_position, cards))
+
+sorted_plays = sorted(sorted_plays, key=lambda x: (x[1], x[2][0], x[2][1], x[2][2], x[2][3], x[2][4] ))
+
+acc = 0
+for i in range(len(sorted_plays)):
+    acc += sorted_plays[i][0] * (i+1)
+
+print(f"Part 1: {acc}")
+
+# part 2
+
+rank["J"] = 0
+sorted_plays = []
+cards_arr = []
+for line in input:
+    cards, bid = line.split(" ")
+    cards = [*cards]
+    bid = int(bid)
+    hand = 0
+    cards_count = {}
+    strength_per_position = []
+    for card in cards:
+        if card in cards_count:
+            cards_count[card] += 1
+        else:
+            cards_count[card] = 1
+        strength_per_position.append(rank[card])
+
+    max = (0, 0)
+    if "J" in cards:
+        card_count_dict = cards_count.items()
+        for card_key, c in card_count_dict:
+            if card_key == "J":
+                continue
+            if c > max[1]:
+                max = (card_key, c)
+    if max[0] != 0:
+        cards_count[max[0]] += cards_count["J"]
+        cards_count["J"] = 0
+
+    card_counts_values = list(cards_count.values())
+    if 5 in card_counts_values:
+        hand = hands["five of a kind"]
+    elif 4 in card_counts_values:
+        hand = hands["four of a kind"]
+    elif 3 in card_counts_values:
+        if 2 in card_counts_values:
+            hand = hands["full house"]
+        else:
+            hand = hands["three of a kind"]
+    elif 2 in card_counts_values:
+        if card_counts_values.count(2) == 2:
+            hand = hands["two pairs"]
+        else:
+            hand = hands["one pair"]
+    else:
+        hand = 0
+    print(f" c:{cards} h:{hand} b:{bid} spp:{strength_per_position}")
+    cards_arr.append((bid, hand, strength_per_position, cards))
+
+    sorted_plays.append((bid, hand, strength_per_position, cards))
+
+sorted_plays = sorted(sorted_plays, key=lambda x: (x[1], x[2][0], x[2][1], x[2][2], x[2][3], x[2][4] ))
+
+acc = 0
+for i in range(len(sorted_plays)):
+    acc += sorted_plays[i][0] * (i+1)
+
+print(f"Part 2: {acc}")
