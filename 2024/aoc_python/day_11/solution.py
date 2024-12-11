05d4d501e1f159abe0d436b0581638ff82012ff5
Erick Daniel Antillón Chinchilla <jpj007.ek@gmail.com>
2024-12-11 12:12:25 -0600

    Implement caching and optimize stone processing for Day 11 solution

--- 2024/aoc_python/day_11/solution.py
+++ 2024/aoc_python/day_11/solution.py
@@ -1,5 +1,19 @@
+import time
+
 
 blinking_times = 75
+
+cache = {}
+
+def get_stones_map(input):
+    stones = [int(x) for x in input.split(" ")]
+    stones_map = {}
+    for stone in stones:
+        if stone in stones_map:
+            stones_map[stone] += 1
+        else:
+            stones_map[stone] = 1
+    return stones_map
 
 def get_stones(stone):
     if stone == 0:
@@ -15,44 +29,32 @@ def get_stones(stone):
         return [stone * 2024]
 
 
-def get_next_blink_status(stone, i, max_blinks):
-    if i == max_blinks:
-        return [stone]
-    if stone == 0:
-        result = get_next_blink_status(1, i + 1, max_blinks)
-        return result
-    elif len(str(stone)) % 2 == 0:
-        stone_str = str(stone)
-        length = len(stone_str)
-        left = int(stone_str[:length // 2])
-        right = int(stone_str[length // 2:])
-        stones = []
-        stones.extend(get_next_blink_status(left, i+1, max_blinks))
-        stones.extend(get_next_blink_status(right, i+1, max_blinks))
-        return stones
-    else:
-        result = get_next_blink_status(stone * 2024, i+1, max_blinks)
-        return result
-
 def part1(input_data):
-    stones = [int(x) for x in input_data.split(" ")]
-    sorted_stones = sorted(stones)
-    total_stones = []
-    for stone in sorted_stones:
-        total_stones.extend(get_next_blink_status(stone, 0, 25))
-    return len(total_stones)
+    stones_map = get_stones_map(input_data)
+    for _ in range(25):
+        result_stones = {}
+        for stone, times in stones_map.items():
+            result = []
+            if stone in cache:
+                result = cache[stone]
+            else:
+                result = get_stones(stone)
+                cache[stone] = result
+            for new_stone in result:
+                if new_stone in result_stones:
+                    result_stones[new_stone] += times
+                else:
+                    result_stones[new_stone] = times
 
+        stones_map = result_stones
+    total = 0
+    for stone, times in stones_map.items():
+        total += times
+    return total
 
 
 def part2(input_data):
-    stones = [int(x) for x in input_data.split(" ")]
-    stones_map = {}
-    for stone in stones:
-        if stone in stones_map:
-            stones_map[stone] += 1
-        else:
-            stones_map[stone] = 1
-
+    stones_map = get_stones_map(input_data)
     for _ in range(blinking_times):
         result_stones = {}
         for stone, times in stones_map.items():
@@ -69,11 +71,17 @@ def part2(input_data):
         total += times
     return total
 
+
 def main():
+    st = time.time()
+
     input_data = open("./input.txt", "r").read()
     print("Solution for day 11")
     print("Part 1: ", part1(input_data))
     print("Part 2: ", part2(input_data))
 
+    et = time.time()
+
+    print("Execution time in seconds: ", et-st)
 
 main()
